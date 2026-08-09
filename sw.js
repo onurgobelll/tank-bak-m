b41f0d1dd4df678370f47eb0441c937dc0a3ed16
onurgobelll <onurgobelll@gmail.com>
2026-08-09 08:34:25 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tanktakip-v4";
+const CACHE = "tanktakip-v5";
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./tank-takip.html'])).then(()=>self.skipWaiting()));
 });
